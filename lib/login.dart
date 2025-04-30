a32e7aa85e56d471fc99983cf162d8d4d5b749ec
Şükrü Kaan Tetik <tetiksukrukaan@gmail.com>
2025-04-30 23:39:19 +0300

    firebase connection

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(JobEaseApp());
 }
 
@@ -17,7 +21,6 @@ class JobEaseApp extends StatelessWidget {
     );
   }
 }
-
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
